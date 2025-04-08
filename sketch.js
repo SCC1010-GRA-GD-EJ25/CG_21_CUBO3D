493ce7fc3ba7c3405345b9ccf6cc8409323a4817
CarlosGarciaOb <127284600+CMgarciaOB@users.noreply.github.com>
2025-04-08 22:34:08 +0000

    v1.0

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
+let foto
+
 function preload() {
   // put preload code here
+
+  foto = loadImage("./images/bobesponja.png")
 }
 
 function setup() {
@@ -10,8 +14,60 @@ function setup() {
 function draw() {
   // put drawing code here
   background(255)
-  rotateX(frameCount * 0.01)
-  rotateY(frameCount * 0.01)
-  rotateZ(frameCount * 0.01)
-  box(150)
+  rotateX(frameCount*0.01)
+
+ piramide(150)
+ fill(255,0,0,0)
+ translate(300,0,0)
+ piramide(60)
+}
+
+function piramide(t){
+textureMode(NORMAL)
+ 
+//base
+  texture(foto)
+  beginShape();
+  vertex(-t,-t,-t,0,0);
+  vertex(t,-t,-t,1,0);
+  vertex(t,t,-t,1,1);
+  vertex(-t,t,-t,0,1);
+  vertex(-t,-t,-t,0,0);
+  endShape()
+  
+  //cara 1
+  texture(foto)
+  beginShape()
+  vertex(0,0,t,0.5,0)
+  vertex(-t,-t,-t,0,1)
+  vertex(t,-t,-t,1,1)
+  vertex(0,0,t,0.5,0)
+  endShape()
+
+  //cara 2
+  texture(foto)
+  beginShape()
+  vertex(0,0,t,0.5,0)
+  vertex(t,-t,-t,1,1)
+  vertex(t,t,-t,0,1)
+  vertex(0,0,t,0.5,0)
+  endShape()
+
+  //cara 3
+  texture(foto)
+  beginShape()
+  vertex(0,0,t,0.5,0)
+  vertex(t,t,-t,1,1)
+  vertex(-t,t,-t,0,1)
+  vertex(0,0,t,0.5,0)
+  endShape()
+
+  //cara 4
+  texture(foto)
+  beginShape()
+  vertex(0,0,t,0.5,0)
+  vertex(-t,t,-t,1,1)
+  vertex(-t,-t,-t,0,1)
+  vertex(0,0,t,0.5,0)
+  endShape()
 }
